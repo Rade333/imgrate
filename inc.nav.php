f04ac22641ed91de2296aff3770ad195d19ad285
Rasmus Werling <rasmus.werling@gmail.com>
2013-03-07 14:39:20 +0200

    Wrapped navigation around <nav> tag

--- inc.nav.php
+++ inc.nav.php
@@ -1,19 +1,21 @@
 <div class="navbar">
   <div class="navbar-inner">
-    <div class="container">      
-      <ul class="nav">
-        <li <?php print currentPage('index'); ?>><a href="index.php">Rate</a></li>
-        <?php if (checkLogin()): ?>
-        <li <?php print currentPage('upload'); ?>><a href="upload.php">Upload</a></li>
-        <?php endif; ?>
-        <li <?php print currentPage('stats'); ?>><a href="stats.php">Stats</a></li>
-        <?php if (!checkLogin()): ?>
-        <li <?php print currentPage('login'); ?>><a href="login.php">Log in</a></li>
-        <?php endif; ?>
-        <?php if (checkLogin()): ?>
-        <li><a href="index.php?do=logout">Log out</a></li>
-        <?php endif; ?>
-      </ul>
+    <div class="container">
+      <nav>     
+        <ul class="nav">
+          <li <?php print currentPage('index'); ?>><a href="index.php">Rate</a></li>
+          <?php if (checkLogin()): ?>
+          <li <?php print currentPage('upload'); ?>><a href="upload.php">Upload</a></li>
+          <?php endif; ?>
+          <li <?php print currentPage('stats'); ?>><a href="stats.php">Stats</a></li>
+          <?php if (!checkLogin()): ?>
+          <li <?php print currentPage('login'); ?>><a href="login.php">Log in</a></li>
+          <?php endif; ?>
+          <?php if (checkLogin()): ?>
+          <li><a href="index.php?do=logout">Log out</a></li>
+          <?php endif; ?>
+        </ul>
+      </nav>
     </div>
   </div>
 </div>
