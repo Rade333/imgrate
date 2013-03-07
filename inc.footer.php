5a443f4b639a1ebe99489c8c8f2d20f95bb4c330
Rasmus Werling <rasmus.werling@gmail.com>
2013-03-07 14:38:37 +0200

    Changed footer to use the <footer> tag

--- inc.footer.php
+++ inc.footer.php
@@ -1,6 +1,5 @@
-<div class="footer">
-  2010 &copy; Rade.fi / Rasmus Werling. All rights reserved.
-</div>
+<footer class="text-center"><small>2010 &copy; Rade.fi / Rasmus Werling. All rights reserved.</small></footer>
+</div> <!-- End container -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="jquery-actions.js"></script>
 <script src="js/bootstrap.min.js"></script>
